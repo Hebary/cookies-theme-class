04c0bcd29553cf626e581d1daeac9423722f8ce3
shakharho <berdaddario@gmail.com>
2023-02-13 08:31:26 -0300

    theme created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 
 
-
-
 interface Props {
 }
-
-export const HomePage: React.FC<Props> = ({}) => {
+const HomePage: React.FC<Props> = ({}) => {
    return (
         <div>
           <h1 className='fadein'>Hola Mundo</h1>
         </div>
     )
 }
+
+
+export default HomePage
